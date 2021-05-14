6d98b61bcdd648a45157d81d13feabb9badd9bb1
Hopeful89 <brandon.stine89@gmail.com>
2021-05-14 17:56:15 -0600

    added changeReturn method to sort bill/change return

--- src/com/skilldistillery/MakeChange.java
+++ src/com/skilldistillery/MakeChange.java
@@ -10,15 +10,15 @@ public class MakeChange {
 
 		MakeChange checkout = new MakeChange();
 
-		checkout.promptCustomer("getItem");
-		double itemPrice = scanner.nextDouble();
-		checkout.promptCustomer("");
-		double tender = scanner.nextDouble();
+//		checkout.promptCustomer("getItem");
+//		double itemPrice = scanner.nextDouble();
+//		checkout.promptCustomer("");
+//		double tender = scanner.nextDouble();
 		
-		checkout.changeReturn(itemPrice, tender);
+//		checkout.changeReturn(itemPrice, tender);
+		checkout.changeReturn(2.96, 20.00);
 
 		// TODO random function if wanted
-		// TODO Display appropriate message if tender less than price ///////////Story 3
 
 		// TODO return change to customer largest to smallest /////////// Story 4
 	}
@@ -33,6 +33,7 @@ public class MakeChange {
 	
 	private boolean verifyEnoughTender(double itemPrice, double tender) {
 		if(tender >= itemPrice) {
+			System.out.println("You have enough money");
 			return true;
 		}
 		System.out.println("Not enough tender provided for purchase.");
@@ -41,7 +42,26 @@ public class MakeChange {
 	
 	private void changeReturn(double itemPrice, double tender) {
 		if(verifyEnoughTender(itemPrice, tender)) {
-			System.out.println("You have enough money");
+			double changeAmount = tender - itemPrice;
+			int tenDollar = (int)changeCounter(changeAmount, 10);
+			changeAmount %= 10;
+			int fiveDollar = (int)changeCounter(changeAmount, 5);
+			changeAmount %= 5;
+			int oneDollar = (int)changeCounter(changeAmount, 1);
+			changeAmount %= 1;
+			int quarter = (int)changeCounter(changeAmount, .25);
+			changeAmount %= .25;
+			int dime = (int)(changeAmount / .10);
+			changeAmount %= .10;
+			int nickel = (int)(changeAmount / .05);
+			changeAmount %= .05;
+			int penny = (int)changeCounter(changeAmount, .01);
+			changeAmount %= .01;
+			System.out.println(penny);
 		}
+	}
+	
+	private double changeCounter(double changeAmount, double modForCash) {
+		return changeAmount / modForCash;
 	}
 }
