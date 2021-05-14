7d6ff5f7a88579b11dc2752acf698dd34f1f8c44
Hopeful89 <brandon.stine89@gmail.com>
2021-05-14 16:59:29 -0600

    added tender verfication method/change return method

--- src/com/skilldistillery/MakeChange.java
+++ src/com/skilldistillery/MakeChange.java
@@ -9,13 +9,14 @@ public class MakeChange {
 	public static void main(String[] args) {
 
 		MakeChange checkout = new MakeChange();
-		
+
 		checkout.promptCustomer("getItem");
 		double itemPrice = scanner.nextDouble();
 		checkout.promptCustomer("");
 		double tender = scanner.nextDouble();
+		
+		checkout.changeReturn(itemPrice, tender);
 
-		// TODO user is prompted again for amount tendered by customer ///////////Story2
 		// TODO random function if wanted
 		// TODO Display appropriate message if tender less than price ///////////Story 3
 
@@ -23,10 +24,24 @@ public class MakeChange {
 	}
 
 	private void promptCustomer(String type) {
-		if(type.equals("getItem")) {
-			System.out.print("Please enter the item price: ");			
-		}else {
-			System.out.print("Please enter tender provided: ");						
+		if (type.equals("getItem")) {
+			System.out.print("Please enter the item price: ");
+		} else {
+			System.out.print("Please enter tender provided: ");
+		}
+	}
+	
+	private boolean verifyEnoughTender(double itemPrice, double tender) {
+		if(tender >= itemPrice) {
+			return true;
+		}
+		System.out.println("Not enough tender provided for purchase.");
+		return false;
+	}
+	
+	private void changeReturn(double itemPrice, double tender) {
+		if(verifyEnoughTender(itemPrice, tender)) {
+			System.out.println("You have enough money");
 		}
 	}
 }
