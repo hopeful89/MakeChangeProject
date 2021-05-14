f5e45a4674c12547e55ad584c3c24d6b02ae89c5
Hopeful89 <brandon.stine89@gmail.com>
2021-05-14 16:50:36 -0600

    added user prompt method

--- src/com/skilldistillery/MakeChange.java
+++ src/com/skilldistillery/MakeChange.java
@@ -1,10 +1,32 @@
 package com.skilldistillery;
 
+import java.util.Scanner;
+
 public class MakeChange {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static Scanner scanner = new Scanner(System.in);
 
+	public static void main(String[] args) {
+
+		MakeChange checkout = new MakeChange();
+		
+		checkout.promptCustomer("getItem");
+		double itemPrice = scanner.nextDouble();
+		checkout.promptCustomer("");
+		double tender = scanner.nextDouble();
+
+		// TODO user is prompted again for amount tendered by customer ///////////Story2
+		// TODO random function if wanted
+		// TODO Display appropriate message if tender less than price ///////////Story 3
+
+		// TODO return change to customer largest to smallest /////////// Story 4
 	}
 
+	private void promptCustomer(String type) {
+		if(type.equals("getItem")) {
+			System.out.print("Please enter the item price: ");			
+		}else {
+			System.out.print("Please enter tender provided: ");						
+		}
+	}
 }
