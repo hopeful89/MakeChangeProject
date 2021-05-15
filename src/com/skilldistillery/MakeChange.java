6d2ddc5b3b8ce89dccb54ad95196052b21f51447
Hopeful89 <brandon.stine89@gmail.com>
2021-05-15 12:11:43 -0600

    Added whitespace for readability

--- src/com/skilldistillery/MakeChange.java
+++ src/com/skilldistillery/MakeChange.java
@@ -11,23 +11,24 @@ public class MakeChange {
 		MakeChange checkout = new MakeChange();
 
 		// PROMPT USER FOR INPUT AND STORE
+
 		boolean enteringItems = true;
-		
-		while(enteringItems) {
+
+		while (enteringItems) {
 			checkout.promptCustomer("getItem");
 			double itemPrice = scanner.nextDouble();
-			
+
 			checkout.promptCustomer("");
 			double tender = scanner.nextDouble();
-			
+
 			checkout.changeReturn(itemPrice, tender);
 			checkout.promptCustomer("continue");
-			
+
 			String customerOption = scanner.next();
-			
-			if(customerOption.equalsIgnoreCase("Y") || customerOption.equalsIgnoreCase("Yes")) {
+
+			if (customerOption.equalsIgnoreCase("Y") || customerOption.equalsIgnoreCase("Yes")) {
 				continue;
-			}else {
+			} else {
 				enteringItems = false;
 			}
 		}
@@ -35,11 +36,11 @@ public class MakeChange {
 	}
 
 	private void promptCustomer(String type) {
-		if(type.equals("continue")){
+		if (type.equals("continue")) {
 			System.out.print("Would you like to enter another item Y | N: ");
-		}else if (type.equals("getItem")) {
+		} else if (type.equals("getItem")) {
 			System.out.print("Please enter the item price: ");
-		}else {
+		} else {
 			System.out.print("Please enter tender provided: ");
 		}
 	}
@@ -47,6 +48,7 @@ public class MakeChange {
 	// CHECKS IF ENOUGH TENDER WAS PROVIDED FOR PURCHASE
 
 	private boolean verifyEnoughTender(double itemPrice, double tender) {
+
 		if (tender >= itemPrice && itemPrice > 0) {
 			return true;
 		}
@@ -58,7 +60,7 @@ public class MakeChange {
 
 	private void changeReturn(double itemPrice, double tender) {
 
-	// VERIFY ENOUGH TENDER FOR PURCHASE //
+		// VERIFY ENOUGH TENDER FOR PURCHASE //
 
 		if (verifyEnoughTender(itemPrice, tender)) {
 
@@ -85,16 +87,16 @@ public class MakeChange {
 			int penny = (int) (changeCounter(changeAmount, .01) + .5);
 			changeAmount %= .01;
 
-	// USER OUTPUT INVOCATION
+			// USER OUTPUT INVOCATION
 
 			userChangeProvided(tenDollar, fiveDollar, oneDollar, quarter, dime, nickel, penny, itemPrice, tender);
 		}
 	}
 
-	// RETURNS MAX BILLS DIVISIBLE
+	// RETURNS MAX BILLS DIVISIBLE 
 
-	private double changeCounter(double changeAmount, double modForCash) {
-		return changeAmount / modForCash;
+	private double changeCounter(double changeAmount, double maxBillSize) {
+		return changeAmount / maxBillSize;
 	}
 
 	// OUTPUTS FINAL RESULT FROM STRING BUILDER TO USER
@@ -105,7 +107,7 @@ public class MakeChange {
 		String finalResult = String.format("Amount: %.2f, Tendered: %.2f, Result:", itemPrice, tender);
 		String tempString = "";
 
-	// CALL STRING BUILDER
+		// CALL STRING BUILDER
 
 		tempString = userChangeStringBuilder(tempString, ten, "ten dollar", " bill,", "bills,");
 		tempString = userChangeStringBuilder(tempString, five, "five dollar", " bill,", "bills,");
@@ -115,7 +117,7 @@ public class MakeChange {
 		tempString = userChangeStringBuilder(tempString, nickel, "nickel,", "", "nickels,");
 		tempString = userChangeStringBuilder(tempString, penny, "penny", "", "pennies");
 
-	// REMOVE COMMAS AT END IF REQUIRED
+		// REMOVE COMMAS AT END IF REQUIRED
 
 		tempString = checkRemoveComma(tempString);
 
@@ -126,24 +128,29 @@ public class MakeChange {
 
 	// PROVIDES STRING FOR OUTPUT
 
-	private String userChangeStringBuilder(String current, int typeOfCurrency, String billType, String billSingular,
+	private String userChangeStringBuilder(String currentString, int typeOfCurrency, String billType, String billSingular,
 			String billPlural) {
 
 		if (typeOfCurrency > 1) {
 
-			if (billType.contains("quarter") || billType.contains("dime") || billType.contains("nickel") || billType.contains("penny")) {
-				current += " " + typeOfCurrency + " " + billPlural;
-				return current;
+			if (billType.contains("quarter") || billType.contains("dime") || billType.contains("nickel")
+					|| billType.contains("penny")) {
+
+				currentString += " " + typeOfCurrency + " " + billPlural;
+				return currentString;
 			} else {
-				current += " " + typeOfCurrency + " " + billType + " " + billPlural;
-				return current;
+
+				currentString += " " + typeOfCurrency + " " + billType + " " + billPlural;
+				return currentString;
 			}
 
 		} else if (typeOfCurrency > 0) {
-			current += " " + typeOfCurrency + " " + billType + billSingular;
-			return current;
+
+			currentString += " " + typeOfCurrency + " " + billType + billSingular;
+			return currentString;
 		} else {
-			return current;
+
+			return currentString;
 		}
 	}
 
